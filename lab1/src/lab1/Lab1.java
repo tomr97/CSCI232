510b28a19221bb0ef1ee9449de5f4913c45d48b0
Tom Rudolph <tom.rudolph310@gmail.com>
2019-01-18 13:21:13 -0700

    Added new project

---
+++ lab1/src/lab1/Lab1.java
@@ -0,0 +1,20 @@
+/*
+ * Author     : Thomas Rudolph
+ * Lab        : 1
+ * Date       : 1/18/218
+ * Description: This lab sets up a priority queue for different jobs that will 
+ *                 
+ */
+package lab1;
+
+
+public class Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
