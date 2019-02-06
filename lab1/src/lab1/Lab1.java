520876067399b20a511c565517b2f2356191d3fa
Tom Rudolph <tom.rudolph310@gmail.com>
2019-02-06 14:20:03 -0700

    Got file to read in

--- lab1/src/lab1/Lab1.java
+++ lab1/src/lab1/Lab1.java
@@ -9,17 +9,24 @@
 package lab1;
 
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import lab1.jobs;
 
 public class Lab1 {
     /***********************************************************************************
      *                               Variables
      **********************************************************************************/
-    static String file      = "commands.txt";
+    static String file       = "C:\\Users\\d92r471\\Documents\\CSCI232\\lab1\\src\\lab1\\commands.txt";
     
-    static Scanner scan     = new Scanner(System.in);
-    static jobs j_queue[]   = new jobs[20]; // Priority queue for jobs
-    static jobs j_waiting[] = new jobs[20]; // Jobs waiting to be placed in queue
+    static BufferedReader br = null;
+    static jobs j_queue[]    = new jobs[30]; // Priority queue for jobs
+    static jobs j_waiting[]  = new jobs[30]; // Jobs waiting to be placed in queue
     
     /***********************************************************************************
      *                               Main Method
@@ -43,12 +50,26 @@ public class Lab1 {
     /***********************************************************************************
      * Function Name : read_file()
      * Input(s)      : file_read - String of the file name
-     * Output        : 
-     * Description   : 
+     * Output        : None
+     * Description   : Function to read in file and set up 
      **********************************************************************************/
     
     private static void read_file(String file_read)
     {
+        try {
+            br = new BufferedReader(new FileReader(file_read));
+        } catch (FileNotFoundException ex) {Logger.getLogger(Lab1.class.getName()).log(Level.SEVERE, null, ex);}
         
+        String line; 
+        int num = 0; // Keeps track of the job number that was just read in
+        try { // Reads in from file
+            while((line = br.readLine()) != null)
+            {
+                String[] c       = line.split(" ");
+                j_waiting[num++] = new jobs(Integer.parseInt(c[0]), Integer.parseInt(c[1]), Integer.parseInt(c[2]), Integer.parseInt(c[3])); 
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Lab1.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- lab1/src/lab1/jobs.java
+++ lab1/src/lab1/jobs.java
@@ -11,7 +11,10 @@ public class jobs {
     /***********************************************************************************
      *                               Variables
      **********************************************************************************/
-    
+    private int job_num;
+    private int priority;
+    private int arrival_time;
+    private int duration;
     
     
     
@@ -22,11 +25,75 @@ public class jobs {
 	    
     /***********************************************************************************
      * Function Name : jobs
-     * Input(s)      : 
+     * Input(s)      : _jobNom      - int to fill job_num
+     *                 _priority    - int to fill priority
+     *                 _arrivalTime - int to fill arrival_time
+     *                 _duration    - int to fill duration
      * Description   : Object to keep track of information about jobs
      **********************************************************************************/
-    public jobs()
+    public jobs(int _jobNom, int _priority, int _arrivalTime, int _duration)
     {
-        
+        job_num      = _jobNom;
+        priority     = _priority;
+        arrival_time = _arrivalTime;
+        duration     = _duration;
+    }
+    
+    /***********************************************************************************
+     *                                Methods
+     **********************************************************************************/
+    /***********************************************************************************
+     * Function Name : get_job_num()
+     * Input(s)      : None
+     * Output        : int - return number of job
+     * Description   : Function to return
+     **********************************************************************************/
+    public int get_job_num()
+    {
+        return job_num;
+    }
+    
+    /***********************************************************************************
+     * Function Name : get_priority()
+     * Input(s)      : None
+     * Output        : int - return priority of job
+     * Description   : Function to return priority of jobs
+     **********************************************************************************/
+    public int get_priority()
+    {
+        return priority;
+    }
+    
+    /***********************************************************************************
+     * Function Name : get_arrival_time()
+     * Input(s)      : None
+     * Output        : int - arrival time of job
+     * Description   : Function to return arrival time of jobs
+     **********************************************************************************/
+    public int get_arrival_time()
+    {
+        return arrival_time;
+    }
+    
+    /***********************************************************************************
+     * Function Name : get_duration()
+     * Input(s)      : None
+     * Output        : int - duration of job
+     * Description   : Function to return duration
+     **********************************************************************************/
+    public int get_duration()
+    {
+        return duration;
+    }
+    
+    /***********************************************************************************
+     * Function Name : print_job()
+     * Input(s)      : None
+     * Output        : None
+     * Description   : Function to print job info
+     **********************************************************************************/
+    public void print_job()
+    {
+        System.out.println(job_num + " " + priority + " " + arrival_time + " " + duration);
     }
 }
