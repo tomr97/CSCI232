f3b93b4b2240d4ba0066302f246053f5ce5e4f88
Tom Rudolph <twrudolph310@gmail.com>
2019-03-06 21:03:45 -0700

    Added lab 2

---
+++ BinPacker/src/binpacker/BinPacker.java
@@ -0,0 +1,17 @@
+/*
+ * Author     : Thomas Rudolph
+ * Lab        : 2
+ * Date       : 3/8/2018
+ * Description: This lab tests various bin packing strategies
+ */
+package binpacker;
+
+
+public class BinPacker {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
