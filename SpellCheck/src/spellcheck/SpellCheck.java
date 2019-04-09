5634cfc7cae89776093e1abc605e40268c8ffa2b
Connor Van Meter <akdreamin21@gmail.com>
2019-04-09 11:17:19 -0600

    Added comments and took out some block comments

--- SpellCheck/src/spellcheck/SpellCheck.java
+++ SpellCheck/src/spellcheck/SpellCheck.java
@@ -1,18 +1,16 @@
 /*******************************************************************************
-*
+ * Authors      : Thomas Rudolph, Connor Van Meter
+ * Lab         : 4
+ * Date        : 4/12/2019
+ * Description : This lab spell checks a file and puts the spell checks words
+ *                  into a new file.
 *******************************************************************************/
 
 package spellcheck;
 
-/**
- *
- * @author Tom
- */
-public class SpellCheck {
 
-    /**
-     * @param args the command line arguments
-     */
+public class SpellCheck {
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
