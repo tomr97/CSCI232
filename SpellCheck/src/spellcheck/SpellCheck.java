81d58aaaba3e3c15358c0f72799eba899628d00c
Connor Van Meter <akdreamin21@gmail.com>
2019-04-09 11:41:15 -0600

    Added file read in method and modified. Not completely done yet.

--- SpellCheck/src/spellcheck/SpellCheck.java
+++ SpellCheck/src/spellcheck/SpellCheck.java
@@ -9,10 +9,45 @@
 package spellcheck;
 
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class SpellCheck {
+
+    private static String file = "C:\\Users\\Tom\\Documents\\CSCI232\\MyGPS\\src\\mygps\\gz.gr";
+    private static String path = "C:\\Users\\Tom\\Documents\\CSCI232\\MyGPS\\src\\mygps\\";
+
+    private static BufferedReader br;
 
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+
+    /***********************************************************************************
+     * Function Name : read_file()
+     * Input(s)      : None
+     * Output        : None
+     * Description   : Function to read in from file
+     **********************************************************************************/
+    private static void read_file()
+    {
+
+        try {
+            br = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        String line;
+
+        /*// Reads in from file
+        while((line = br.readLine()) != null)
+        {
+            String[] c       = line.split(" ");
+        }*/
+
     }
     
 }
