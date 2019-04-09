9cd0c96437a248c93b3815b0d96fe940d8427f2f
Tom Rudolph <twrudolph310@gmail.com>
2019-04-09 11:27:16 -0600

    Added node class for linked list

--- SpellCheck/src/spellcheck/SpellCheck.java
+++ SpellCheck/src/spellcheck/SpellCheck.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Authors      : Thomas Rudolph, Connor Van Meter
+ * Authors     : Thomas Rudolph, Connor Van Meter
  * Lab         : 4
  * Date        : 4/12/2019
  * Description : This lab spell checks a file and puts the spell checks words
---
+++ SpellCheck/src/spellcheck/word.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spellcheck;
+
+
+public class word {
+    
+    private String word;
+    private word nextWord;
+    
+    public word(String _w)
+    {
+        word = _w;
+    }
+    
+    /***********************************************************************************
+     * Function Name : getNext()
+     * Input(s)      : None
+     * Output        : Word variable to say what the next word is
+     * Description   : Getter function for next node in linked list
+     * 
+     **********************************************************************************/
+    public word getNext()
+    {
+        return nextWord;
+    }
+    
+    /***********************************************************************************
+     * Function Name : getWord()
+     * Input(s)      : None
+     * Output        : String variable for word
+     * Description   : Getter function for word
+     **********************************************************************************/
+    public String getWord()
+    {
+        return word;
+    }
+    /***********************************************************************************
+     * Function Name : setNext()
+     * Input(s)      : _n - word variable to say next node in linked list
+     * Output        : None
+     * Description   : Setter function for next node in linked list 
+     **********************************************************************************/
+    public void setNext(word _n)
+    {
+        nextWord = _n;
+    }
+}
